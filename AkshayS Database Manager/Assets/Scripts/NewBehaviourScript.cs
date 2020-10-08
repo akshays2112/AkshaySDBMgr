262c235bd2fbadc75cb9f0337fedd64527a3a502
Akshay Srinivasan <35820570+akshays2112@users.noreply.github.com>
2020-10-09 02:07:10 +0530

    SQL Server and MySql working

--- AkshayS Database Manager/Assets/Scripts/NewBehaviourScript.cs
+++ AkshayS Database Manager/Assets/Scripts/NewBehaviourScript.cs
@@ -1,34 +1,22 @@
 ﻿using UnityEngine;
-//using Microsoft.Data.SqlClient;
 using UnityEngine.UI;
-using SQLServerUnityLib;
+//using Npgsql;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        SQLServerConnProps sqlServerConnProps = new SQLServerConnProps();
-        sqlServerConnProps.serverName = "192.168.29.224";
-        sqlServerConnProps.userID = "sa";
-        SQLServerConn sqlServerConn = new SQLServerConn(sqlServerConnProps);
-        SQLServerUtilities sqlServerUtilities = new SQLServerUtilities();
-        QueryResult queryResult = sqlServerUtilities.GetQueryResult(sqlServerConn, "select Name from Recipes");
-        gameObject.GetComponent<Text>().text = queryResult.queryResultRows[0].columnData[0];
+        //NpgsqlConnection npgsqlConnection = new NpgsqlConnection("Host=127.0.0.1;Database=akshaysdemodb;Username=postgres;Password=P@ssword123");
+        //npgsqlConnection.Open();
+        //NpgsqlCommand npgsqlCommand = new NpgsqlCommand("select * from recipes", npgsqlConnection);
+        //NpgsqlDataReader npgsqlDataReader = npgsqlCommand.ExecuteReader();
+        //while (npgsqlDataReader.Read())
+        //{
+        //    gameObject.GetComponent<Text>().text = npgsqlDataReader["name"].ToString();
+        //    break;
+        //}
+        //npgsqlDataReader.Close();
+        //npgsqlConnection.Close();
     }
-
-    //void SimpleTestSQLServer()
-    //{
-    //    SqlConnection sqlConnection = new SqlConnection("Server=192.168.29.224;Database=AkshaySDemoDB;User=sa;Password=P@ssword123;");
-    //    sqlConnection.Open();
-    //    SqlCommand sqlCommand = new SqlCommand("select name from recipes", sqlConnection);
-    //    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
-    //    while (sqlDataReader.Read())
-    //    {
-    //        gameObject.GetComponent<Text>().text = sqlDataReader["Name"].ToString();
-    //        break;
-    //    }
-    //    sqlDataReader.Close();
-    //    sqlConnection.Close();
-    //}
 }
